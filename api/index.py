0dc2863a63d0807c80d5c06fdab7a53c8045995d
lRozx <1010137068@u.icesi.edu.co>
2023-03-12 16:10:24 -0500

    Finished

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 import openai
 
 app = Flask(__name__, template_folder='templates')
@@ -14,9 +15,16 @@ def submit():
 
     open_ai_cookie = request.cookies.get("not-api-cookie")
 
-    openai.api_key = f"{open_ai_cookie}"
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{request.json['inputValue']}"}])
-    return jsonify({'message': completion.choices[0].message.content})
+    if not open_ai_cookie:
+        return jsonify({'message': 'No OpenAI key, check https://platform.openai.com/account/api-keys for your key'})
+    else:
+        try:
+            openai.api_key = f"{open_ai_cookie}"
+            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{request.json['inputValue']}"}])
+            return jsonify({'message': completion.choices[0].message.content})
+        except requests.exceptions.RequestException as err:
+            print('Something went wrong:', err)
+            return jsonify({'message': 'Something went wrong'}), 500
 
 
 if __name__ == '__main__':
